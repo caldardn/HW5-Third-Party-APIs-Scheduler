164bc2c6ac638778181420a4e4a97e675c02ce66
caldardn <caldardn@gmail.com>
2023-09-24 18:51:36 -0400

    Adding function that resets localstorage and added comments

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,13 @@
 let presentDay = dayjs();
-let displayTime = dayjs().format("hh:mm A");
 let currentTime = dayjs().format("HH");
-let refreshBtn = $("#refresh");
 const save = $(".saveBtn");
 let todo = [];
 
-$("#currentDay").text(presentDay.format("dddd, MMMM D"));
+//displays the current date
+$("#currentDay").text(presentDay.format("dddd, MMMM D YYYY"));
 
-const submit = function () {
-  // todo array
+//Gets the value entered by the user when they click the save button and saves it as an array.
+function submit() {
   todo[0] = $("#hr9").val();
   todo[1] = $("#hr10").val();
   todo[2] = $("#hr11").val();
@@ -18,17 +17,15 @@ const submit = function () {
   todo[6] = $("#hr3").val();
   todo[7] = $("#hr4").val();
   todo[8] = $("#hr5").val();
-  console.log(todo[0]);
-  // Stringify and set key in localStorage to todo array
-  localStorage.setItem("list", JSON.stringify(todo));
-};
+  //stores the data the user subits as an array in localstorage.
+  localStorage.setItem("todoList", JSON.stringify(todo));
+}
 
 function init() {
-  // Computer gets stored results from localStorage.
-  let todoActivity = JSON.parse(localStorage.getItem("list"));
-  // If todo were retrieved from localStorage, update the todo array to it.
-  if (todoActivity !== null) {
-    todo = todoActivity;
+  // Computer gets the stored results from localStorage.
+  let todo = JSON.parse(localStorage.getItem("todoList"));
+  // If a todo was retrieved from localStorage it will update the todo array to it.
+  if (todo !== null) {
     $('textarea[id="hr9"]').val(todo[0]);
     $('textarea[id="hr10"]').val(todo[1]);
     $('textarea[id="hr11"]').val(todo[2]);
@@ -38,10 +35,10 @@ function init() {
     $('textarea[id="hr3"]').val(todo[6]);
     $('textarea[id="hr4"]').val(todo[7]);
     $('textarea[id="hr5"]').val(todo[8]);
-    $('textarea[id="hr8"]').val(todo[9]);
   }
 }
 
+//This selects all the row classes gets the id attribute of the current element and converts it to an integer. It uses that integer to compare the currentTime to see which class to add.
 $(document).ready(function () {
   $(".row").each(function () {
     let timeSlot = $(this).attr("id");
@@ -59,6 +56,8 @@ $(document).ready(function () {
   });
 });
 
+
+//displays the current time to the user
 function updateTime() {
   let clockElement = $("#currentTime");
   clockElement.text("Current Time: " + dayjs().format("hh:mm:ss A"));
@@ -66,15 +65,25 @@ function updateTime() {
 
 setInterval(updateTime, 1000);
 
-function refreshPage(){
-  let nextHour = presentDay.add(1, 'hour').startOf('hour')
-  let timeToNextHour = nextHour.diff(presentDay)
-  setTimeout(function(){
-    location.reload()
-  }, timeToNextHour)
+//reloads the page after evvery hour so that the color for the past, present, and future will update
+function refreshPage() {
+  let nextHour = presentDay.add(1, "hour").startOf("hour");
+  let timeToNextHour = nextHour.diff(presentDay);
+  setTimeout(function () {
+    location.reload();
+  }, timeToNextHour);
 }
 
+//Clears localstorage everyday for the new day
+function resetData() {
+  if (currentTime >= 24) {
+    localStorage.clear();
+  }
+}
+resetData();
+
+//When th user clicks the save button starts the submit function
 save.on("click", submit);
 
 init();
-refreshPage()
+refreshPage();
